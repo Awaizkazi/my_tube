1a9d0672b2249593e66a7dfbf08a5373e36a17c2
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-05 16:09:58 +0530

    c

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper_view/flutter_swiper_view.dart';
+import 'package:my_tube/screens/content_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Stack(
+            children: [
+              // We need Swiper for Every content
+              Swiper(
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return ContentScreen(
+                    src: 'https://wallpaper.dog/large/20492984.jpg',
+                  );
+                },
+              ),
+              // ContentScreen(src: '',),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'MyTube',
+                      style: GoogleFonts.poppins(
+                          fontSize: 22, fontWeight: FontWeight.w600),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.photo_camera, size: 30),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
