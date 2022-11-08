98fb817b7d78c2a354ef4c0a8dc90e6eec7376cc
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-08 15:29:22 +0530

    c

--- lib/screens/options_screens.dart
+++ lib/screens/options_screens.dart
@@ -1,40 +1,79 @@
 import 'package:flutter/material.dart';
 
 class OptionsScreen extends StatelessWidget {
-  const OptionsScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        SizedBox(),
-        Row(
-          children: [
-            SizedBox(height: 110),
-            Row(
-              children: [
-                CircleAvatar(
-                  child: Icon(
-                    Icons.person,
-                    size: 18,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  SizedBox(height: 110),
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        child: Icon(Icons.person, size: 18),
+                        radius: 16,
+                      ),
+                      SizedBox(width: 6),
+                      Text('Flutter Developer'),
+                      SizedBox(width: 10),
+                      Icon(Icons.verified, size: 15),
+                      SizedBox(width: 6),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Follow',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  radius: 18,
-                ),
-                Text('Flutter_developer'),
-                Icon(Icons.verified, size: 15),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Follow',
-                    style: TextStyle(color: Colors.white),
+                  SizedBox(width: 6),
+                  Text('Flutter is beautiful and Fast 💙❤💛 ..'),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.music_note,
+                        size: 15,
+                      ),
+                      Text('Original Audio - some music track--'),
+                    ],
                   ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ],
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.favorite_outline),
+                  Text('80k'),
+                  SizedBox(height: 20),
+                  Icon(Icons.comment_rounded),
+                  Text('1223'),
+                  SizedBox(height: 20),
+                  Transform(
+                    transform: Matrix4.rotationZ(5.6),
+                    child: Icon(Icons.send),
+                  ),
+                  SizedBox(height: 15),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 80),
+                    child: Icon(Icons.more_vert),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
