0917fd71ec389c98100f15ddf6a7f2bac301142a
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-08 21:26:11 +0530

    c

--- lib/screens/content_screen.dart
+++ lib/screens/content_screen.dart
@@ -1,19 +1,53 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:my_tube/screens/options_screens.dart';
+import 'package:video_player/video_player.dart';
 
-class ContentScreen extends StatelessWidget {
+class ContentScreen extends StatefulWidget {
   final String src;
   const ContentScreen({super.key, required this.src});
+
+  @override
+  State<ContentScreen> createState() => _ContentScreenState();
+}
+
+class _ContentScreenState extends State<ContentScreen> {
+  late VideoPlayerController _videoPlayerController;
+  late ChewieController _chewieController;
+  @override
+  void initState() {
+    initializePlayer();
+  }
+
+  Future initializePlayer() async {
+    _videoPlayerController = VideoPlayerController.network(widget.src);
+    await Future.wait(
+      [_videoPlayerController.initialize()],
+    );
+    _chewieController = ChewieController(
+        videoPlayerController: _videoPlayerController, autoPlay: true);
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       fit: StackFit.expand,
       children: [
-        Image.network(
-          src,
-          fit: BoxFit.cover,
-        ),
+        //  Welcome back to second page
+        // In this part let's actual video player and double tap
+
+        _chewieController != null &&
+                _chewieController.videoPlayerController.value.isInitialized
+            ? Chewie(
+                controller: _chewieController,
+              )
+            : SizedBox(),
         OptionsScreen(),
       ],
     );
